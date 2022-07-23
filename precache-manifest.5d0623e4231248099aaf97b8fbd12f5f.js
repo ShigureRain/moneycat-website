5afcce8df0fd1ef545d6d1e1d1af52be47d219a7
Rain <448130604@qq.com>
2022-07-23 18:21:11 +0800

    deploy

---
+++ precache-manifest.5d0623e4231248099aaf97b8fbd12f5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3103167c58fa21c9bdc6",
+    "url": "/moneycat-website/css/app.18733266.css"
+  },
+  {
+    "revision": "6981300ef6d6138674bd4d4fb506eaca",
+    "url": "/moneycat-website/index.html"
+  },
+  {
+    "revision": "3103167c58fa21c9bdc6",
+    "url": "/moneycat-website/js/app.0ce02464.js"
+  },
+  {
+    "revision": "a79f8e4be546bcaee882",
+    "url": "/moneycat-website/js/chunk-vendors.afe895f1.js"
+  },
+  {
+    "revision": "2a4550e1d132e9f75ecb2592ebfb80d3",
+    "url": "/moneycat-website/manifest.json"
+  },
+  {
+    "revision": "86ad98aa670720e5aa8db9d7df98e51e",
+    "url": "/moneycat-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moneycat-website/robots.txt"
+  }
+]);
